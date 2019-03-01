1022b71e5953319e3c43b5f8e25c8d07d8ab47c8
Faith <25356566+faithshatt0@users.noreply.github.com>
2019-03-01 14:14:53 -0800

    Simple 3D render

---
+++ Assets/Scripts/destroyer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyer : MonoBehaviour
+{
+    public float lifeTime = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lifeTime > 0)
+        {
+            lifeTime -= Time.deltaTime;
+            if (lifeTime <= 0)
+            {
+                Destruction();
+            }
+        }
+
+        if (this.transform.position.y <= -20)
+        {
+            Destruction();
+        }
+    }
+
+    void OnCollisionEnter(Collision call)
+    {
+        if(call.gameObject.name == "destoyer")
+        {
+            Destruction();
+        }
+    }
+
+    void Destruction()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    //public Transform SpawnPos;
+    public GameObject platform;
+    public GameObject box;
+
+    // Update is called once per frame
+    void Start()
+    {
+        //Instantiate(spawnee, SpawnPos.position, SpawnPos.rotation);
+        Instantiate(platform);
+        Instantiate(box);
+    }
+}
