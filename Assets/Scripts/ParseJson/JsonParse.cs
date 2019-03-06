261fa3d723506bc190877f0856d6d76314613fdb
memej <adiaz@csumb.edu>
2019-03-06 14:46:31 -0800

    JsonParse style updates

--- Assets/Scripts/ParseJson/JsonParse.cs
+++ Assets/Scripts/ParseJson/JsonParse.cs
@@ -1,10 +1,10 @@
 ﻿[System.Serializable]
 public class JsonParse
-{
+    {
     public EthConnection[] eth_clients;    // eth_clients
     public StaConnection[] sta_clients;        // sta_clients
     public MeshLink[] mesh_links;       // mesh_links
-}
+    }
 
 [System.Serializable]
 public class EthConnection : Connection<Eth> { }
@@ -14,15 +14,15 @@ public class StaConnection : Connection<Sta> { }
 
 [System.Serializable]
 public class Connection<T>
-{
+    {
     public T[] clients;
     public string serial;
-}
+    }
 
 // JSON Objects
 [System.Serializable]
 public class Eth
-{
+    {
     /*
       Example Values      
         'idle': '9.10',
@@ -30,19 +30,19 @@ public class Eth
     */
     public string idle;         // Made into string to store exact numbers = "9.10" vs. if double -> value = 9.1000000012
     public string target_mac;
-}
+    }
 
 [System.Serializable]
 public class MeshLink
-{
+    {
     public Device[] connected_to;         // connected_to
     public bool isMaster;
     public string serial;
-}
+    }
 
 [System.Serializable]
 public class Sta
-{
+    {
     /*
      Example Values
         'rssi': -300,
@@ -54,11 +54,11 @@ public class Sta
     public int rxpr;
     public string target_mac;
     public int txpr;
-}
+    }
 
 [System.Serializable]
 public class Device
-{
+    {
     public int rssi;
     public string serial;
-}
+    }
