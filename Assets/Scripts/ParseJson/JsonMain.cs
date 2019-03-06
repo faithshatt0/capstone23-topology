4eb18712944e7a0b8275b3354eddf512b4d90095
memej <adiaz@csumb.edu>
2019-03-06 14:44:33 -0800

    JsonMain styling changes

--- Assets/Scripts/ParseJson/JsonMain.cs
+++ Assets/Scripts/ParseJson/JsonMain.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JsonMain : MonoBehaviour
-{
+    {
 
     // Start is called before the first frame update
     void Start()
-    {
+        {
         // Algorithm:
         //  1. Use JsonUtility to store JSON values in objects -> arrays
         //  2. Store devices based on their respective Router/Extender
@@ -27,11 +27,8 @@ public class JsonMain : MonoBehaviour
         // Main Objects
         //  - network_devices   = Devices connected to the Router/Extender
         //  - serials           = Router references
-        List<Topology> network_devices;
-        List<string> serials;
-
-        network_devices = new List<Topology>();
-        serials = new List<string>();
+        List<Topology> network_devices = new List<Topology>();
+        List<string> serials = new List<string>();
 
         // 2. Store devices based on their respective Router/Extender
         StartParse(loaded_data, ref network_devices, ref serials);
@@ -39,30 +36,29 @@ public class JsonMain : MonoBehaviour
         // 3. Test data
         Debug.Log("Testing Class");
         for (int i = 0; i < network_devices.Count; i++)
-        {
-        Debug.Log(network_devices[i].get_serial() + "\n Eth_client: " + network_devices[i].get_eth_client_idle() + "  " + network_devices[i].get_eth_client_target_mac()
-            + "\n isMaster: " + network_devices[i].get_isMaster() + "\n IS CONNECTED TO MESH_LINKS: " + network_devices[i].print_mesh_link_cto_rssi()
-            + " " +network_devices[i].print_mesh_link_cto_serial() + "\n Sta_clients: " + network_devices[i].print_sta_client_rssi() + " " + network_devices[i].print_sta_client_rxpr() + " " +
-            network_devices[i].print_sta_client_target_mac() + " " + network_devices[i].print_sta_client_txpr());
+            {
+            Debug.Log(network_devices[i].get_serial() + "\n Eth_client: " + network_devices[i].get_eth_client_idle() + "  " + network_devices[i].get_eth_client_target_mac()
+                + "\n isMaster: " + network_devices[i].get_isMaster() + "\n IS CONNECTED TO MESH_LINKS: " + network_devices[i].print_mesh_link_cto_rssi()
+                + " " +network_devices[i].print_mesh_link_cto_serial() + "\n Sta_clients: " + network_devices[i].print_sta_client_rssi() + " " + network_devices[i].print_sta_client_rxpr() + " " +
+                network_devices[i].print_sta_client_target_mac() + " " + network_devices[i].print_sta_client_txpr());
+            }
         }
-    }
 
     // References Functions in 'Functions.cs'
     void StartParse(JsonParse loaded_data, ref List<Topology> network_devices, ref List<string> serials)
-    {
+        {
         Functions temp = new Functions();
         temp.parse_json(loaded_data, ref network_devices, ref serials);
-    }
-
+        }
     void PrintTopology(JsonParse loaded_data)
-    {
+        {
         Functions temp = new Functions();
         temp.print_topology(loaded_data);
-    }
+        }
 
     // Update is called once per frame
     void Update()
-    {
+        {
 
+        }
     }
-}
