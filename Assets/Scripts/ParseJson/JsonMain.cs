9ba49b6a15a3a0807400e45a23e125184f6df6eb
resteybar <resteybar@csumb.edu>
2019-03-05 23:41:11 -0800

    Changed the name of ParseJsonAj.cs to JsonMain.cs

--- Assets/Scripts/ParseJson/JsonMain.cs
+++ Assets/Scripts/ParseJson/JsonMain.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParseJsonAj : MonoBehaviour
+public class JsonMain : MonoBehaviour
 {
 
     // Start is called before the first frame update
