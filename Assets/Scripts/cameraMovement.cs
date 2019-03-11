22f635c5550cb6b3ba33d36461f469e4fb76da87
memej <adiaz@csumb.edu>
2019-03-11 15:35:01 -0700

    camera move and drag

--- Assets/Scripts/cameraMovement.cs
+++ Assets/Scripts/cameraMovement.cs
@@ -8,22 +8,19 @@ public class cameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(1))
         {
             dragOrigin = Input.mousePosition;
             return;
         }
-        if (!Input.GetMouseButton(0)) return;
+        if (!Input.GetMouseButton(1)) return;
 
         Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
         Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
 
         transform.Translate(move, Space.World);
 
-        if (Input.GetMouseButtonDown(1))
-        {
-            return;
-        }
+        
 
     }
 }
