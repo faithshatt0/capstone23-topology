debf551ce4757c1a5bfe5ffb0abbc640ef012a45
memej <adiaz@csumb.edu>
2019-04-08 17:35:21 -0700

    finished hotspot

--- Assets/Scripts/connectionStatus.cs
+++ Assets/Scripts/connectionStatus.cs
@@ -9,23 +9,27 @@ public class connectionStatus : MonoBehaviour
     public Sprite good;
     public Sprite medium;
     public Sprite bad;
+    public Sprite empty;
+    public Sprite ethernet;
+    bool isShowing;
+    public Text score;
+    Sprite prevSprite;
+    float RSSI;
 
-
-    public float min_rssi(List<Device> connected_to)
+    public float average(List<Device> connected_to)
         {
-        int min = 1000000000;
+        float average = 0;
         for(int i = 0; i < connected_to.Count; i++)
             {
-            if(min < connected_to[i].rssi)
-                {
-                min = connected_to[i].rssi;
-                }
+            average += connected_to[i].rssi;
             }
-        return min/10f;
+        Debug.Log(average / connected_to.Count);
+        return average/connected_to.Count;
         }
 
     void Start()
-    {
+        {
+        isShowing = false;
         //Excellent: >= -59; -68 <= Good <= -60; -78 <= Acceptable <= -69; Bad < -78
         //Fetch the Image from the GameObject
         m_Image = GetComponent<Image>();
@@ -42,24 +46,41 @@ public class connectionStatus : MonoBehaviour
         for (int i = 0; i < network_devices.Count; i++)
         {
             
+
+            for(int ii = 0; ii < network_devices[i].get_eth_clients().Count; ii++)
+                {
+                if(m_Image.gameObject.transform.parent.parent.name == network_devices[i].get_eth_clients()[ii].target_mac)
+                    {
+                    m_Image.sprite = ethernet;
+                    prevSprite = ethernet;
+                    }
+                }
+
+
             for (int ii = 0; ii < network_devices[i].get_mesh_links().Count; ii++)
             {
                 
                 if (m_Image.gameObject.transform.parent.parent.name == network_devices[i].get_serial())
                 {
-                    float min_Rssi = min_rssi(network_devices[i].get_mesh_links()[ii].connected_to);
+                    float min_Rssi = average(network_devices[i].get_mesh_links()[ii].connected_to)/10f;
+                    RSSI = min_Rssi;
+                    
                     if (min_Rssi  >= -68f)
-                    {
+                        {
                         m_Image.sprite = good;
-                    }
+                        prevSprite = good;
+
+                        }
                     else if (min_Rssi / 10f <= -69f)
-                    {
+                        {
                         m_Image.sprite = medium;
-                    }
+                        prevSprite = medium;
+                        }
                     else
-                    {
+                        {
                         m_Image.sprite = bad;
-                    }
+                        prevSprite = bad;
+                        }
                 }
             }
             
@@ -76,14 +97,20 @@ public class connectionStatus : MonoBehaviour
                         if(network_devices[i].get_sta_clients()[ii].rssi/10f  >= -68f)
                             {
                             m_Image.sprite = good;
+                            prevSprite = good;
+                            RSSI = network_devices[i].get_sta_clients()[ii].rssi / 10f;
                             }   
                         else if(network_devices[i].get_sta_clients()[ii].rssi / 10f >= -78f && network_devices[i].get_sta_clients()[ii].rssi / 10f <= -69f)
-                           {
+                            {
                             m_Image.sprite = medium;
-                           }
+                            prevSprite = medium;
+                            RSSI = network_devices[i].get_sta_clients()[ii].rssi / 10f;
+                            }
                         else
                             {
                             m_Image.sprite = bad;
+                            prevSprite = bad;
+                            RSSI = network_devices[i].get_sta_clients()[ii].rssi / 10f;
                             }     
                     }
                 }
@@ -95,5 +122,25 @@ public class connectionStatus : MonoBehaviour
 
     }
 
+    //Once the button is chosen it will change what the Sprite is showing
+    public void ButtonInteract()
+        {
+        if(isShowing == true && prevSprite != ethernet)
+            {
+            m_Image.sprite = prevSprite;
+            score.text = "";
+            isShowing = false;
+            }
+        else if(isShowing == false && prevSprite != ethernet)
+            {
+            m_Image.sprite = empty;
+            score.text = RSSI.ToString();
+            isShowing = true;
+            }
+           
+        }
+
     
+
+
 }
---
+++ Assets/Scripts/getRssiNumber.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class getRssiNumber : MonoBehaviour
+{
+    Image m_Image;
+    //Set this in the Inspector
+    public Sprite empty;
+    public Text score;
+    public bool isShowing;
+
+    public float min_rssi(List<Device> connected_to)
+    {
+        int min = 1000000000;
+        for (int i = 0; i < connected_to.Count; i++)
+        {
+            if (min < connected_to[i].rssi)
+            {
+                min = connected_to[i].rssi;
+            }
+        }
+        return min / 10f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+        {
+        isShowing = false;
+        //Excellent: >= -59; -68 <= Good <= -60; -78 <= Acceptable <= -69; Bad < -78
+        //Fetch the Image from the GameObject
+        m_Image = GetComponent<Image>();
+
+        // Initialize JsonMain script and start parsing Json
+        JsonMain jsonMain = new JsonMain();
+        jsonMain.Start();
+
+        // Retrieve network_devices and serials from JsonMain
+        List<Topology> network_devices = jsonMain.GetDevices();
+        List<string> serials = jsonMain.GetSerials();
+        }
+
+    // Update is called once per frame
+    void Update()
+        {
+        
+        }
+    
+
+    
+}
