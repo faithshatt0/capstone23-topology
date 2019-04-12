85991850fe9d389ac17b5881b2db4bb6abbe4e16
memej <adiaz@csumb.edu>
2019-04-12 16:46:39 -0700

    POV

--- Assets/Scripts/A_DInfo.cs
+++ Assets/Scripts/A_DInfo.cs
@@ -8,8 +8,9 @@ using UnityEngine.UI;
 /// Why moving Informational panel instead of using .active() function
 /// When we use the .active() function for a gameobject it will disappear like you want, but if you bring it back to being active it will be super blurry. 
 /// We don't want it to be blurry so it is just moved to a far off location to give the impression of appearing and disappearing.
+/// 
 public class A_DInfo : MonoBehaviour
-{
+    {
     private GameObject target;
     private bool _isOpen;
     public Canvas myChildObject;
@@ -17,46 +18,34 @@ public class A_DInfo : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
+        {
         _isOpen = false; //flag to make only one info panel open
-    }
+        }
 
     // Update is called once per frame
     public void ButtonInteract()
-    {
-
-        Debug.Log(butt.gameObject.transform.parent.parent.name);
-
+        {
+    
         //Finds the informational panel in the heirarchy of the object
         if (butt.gameObject.transform.parent.parent.Find("Informational Panel") != null && _isOpen == false)
-        {
+            {
             if (butt.transform.parent.parent.tag == "router") //brings informational panel location to router
-            {
+                {
                 butt.gameObject.transform.parent.parent.Find("Informational Panel").position = new Vector3(butt.gameObject.transform.parent.parent.Find("Informational Panel").position.x, butt.gameObject.transform.parent.parent.Find("Informational Panel").position.y, butt.gameObject.transform.parent.parent.Find("Informational Panel").parent.position.z);
                 _isOpen = true;
-            }
+                }
             else //brings the informational panel to the devices
-            {
+                {
                 butt.gameObject.transform.parent.parent.Find("Informational Panel").position = new Vector3(butt.gameObject.transform.parent.parent.Find("Informational Panel").position.x, butt.gameObject.transform.parent.parent.Find("Informational Panel").position.y, butt.gameObject.transform.parent.parent.Find("Informational Panel").parent.position.z);
                 _isOpen = true;
+                }
             }
-
-        }
 
         //if the object is clicked again it will move the informational panel to a far off location to give impression of spawning
         else if (butt.gameObject.transform.parent.parent.Find("Informational Panel") != null && _isOpen == true)
-        {
+            {
             _isOpen = false;
             butt.gameObject.transform.parent.parent.Find("Informational Panel").position = new Vector3(butt.gameObject.transform.parent.parent.Find("Informational Panel").position.x, butt.gameObject.transform.parent.parent.Find("Informational Panel").position.y, -10000);
+            }
         }
-
     }
-
-
-
-    
-
-
-}
-
-
---
+++ Assets/Scripts/birdsEye.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class birdsEye : MonoBehaviour
+{
+    public Button butt; //connects button we push
+    public Camera cam; //attach camera
+    public Text text; //attach text
+   
+
+    // Start is called before the first frame update
+    void Start()
+        {
+        text.text = "Normal";
+        }
+
+    // Update is called once per frame
+    public void ButtonInteract()
+        {
+        //Change camera view here not in Start()!
+        //Normal View
+        if(text.text == "Normal")
+            {
+            text.text = "Birdseye";
+            }
+        //Birdseye View
+        else
+            {
+            text.text = "Normal";
+            }
+        }         
+}
