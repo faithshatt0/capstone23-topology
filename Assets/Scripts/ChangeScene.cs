352c7c0ff07a14a65dd3bdc751cc2d104bb90956
Raeleen Watson <raeleenwatson@Raeleens-MacBook-Pro.local>
2019-04-22 22:58:13 -0700

    Need to change objs position, get router go back btn clickable

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeScene : MonoBehaviour
+{
+    public int num;
+    public Button button;
+    public string m_Scene = "viewObject_scene";
+    public GameObject m_MyGameObject;
+    public Text text;
+
+    private Button real;
+
+    private void Start()
+    {
+        //Adds a listener to the to the Go Back button
+        real = GetComponent<Button>();
+        real.onClick.AddListener(buttonWasClicked);
+
+    }
+
+    //If Go Back button in viewObject_Scene was clicked then destroy game object and reload test_scene
+    void buttonWasClicked()
+    {
+
+        if (SceneManager.GetSceneAt(0).name.Equals("viewObject_scene"))
+        {
+            Object.Destroy(m_MyGameObject);
+
+            SceneManager.LoadScene("test_scene");
+        }
+    }
+
+
+    public void ChangeToObjScene()
+    {
+
+        //When the View assistant button is clicked in test_scenee it will load the new scene
+        SceneManager.LoadScene("viewObject_scene");
+
+        var nameOfObj = button.gameObject.transform.parent.parent.name;
+
+        //Change the name of the button
+        text.text = "Go Back";
+
+        //Creating a clone of the game object to be used in the new scene
+        DontDestroyOnLoad(m_MyGameObject);
+
+        //Locatin for the game object
+        float xCord = 0;
+        float yCord = 0;
+        float zCord = 15;
+        m_MyGameObject.transform.position = new Vector3(xCord, yCord, zCord);
+
+    }
+
+}
