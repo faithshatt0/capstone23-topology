da47422624358684f5f05011d7a5dca255011817
Faith <25356566+faithshatt0@users.noreply.github.com>
2019-04-24 22:52:41 -0700

    Added zoom capability on mouse scroll

--- Assets/Scripts/cameraMovement.cs
+++ Assets/Scripts/cameraMovement.cs
@@ -8,26 +8,35 @@ using UnityEngine.UI;
 /// 
 
 public class cameraMovement : MonoBehaviour
-    {
+{
     public float dragSpeed = 2; 
     private float angle;
     public Text text;
     float camZoom = -10f;
     float camZoomSpeed = 2f;
 
+    float minFov = 15f;
+    float maxFov = 90f;
+    float sensitivity = 10f;
+    float fov;
+
     // Update is called once per frame
     void Update()
+    {
+        fov = Camera.main.fieldOfView;
+        fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity * -1;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        Camera.main.fieldOfView = fov;
+
+        if (text.text == "Birdseye")
         {
-        if(text.text == "Birdseye")
-            {
             if (Input.GetMouseButtonDown(1))
-                {
+            {
                 angle = 60 * (Input.mousePosition.x < Screen.width / 2 ? 1 : -1) * Time.deltaTime;
                 return;
-                }
+            }
             if (!Input.GetMouseButton(1)) return;
             transform.RotateAround(Vector3.zero, Vector3.up, angle);
         }
-    }
-       
-    }
+    }      
+}
